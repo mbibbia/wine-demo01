5848aa3837822e041aedd60520120a8b3f7c8579
marco <marco@bibbias-XPS-13>
2018-12-16 11:58:59 +0100

    New Spring Framework with Maven

---
+++ WineDemo01/src/main/java/ch/bibbia/demowine01/DemoWine01Application.java
@@ -0,0 +1,14 @@
+package ch.bibbia.demowine01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWine01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWine01Application.class, args);
+	}
+
+}
+
